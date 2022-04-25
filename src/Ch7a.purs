0e459884e353a46ce287f34fd7d2f87f460cd10f
patal <patourasalexandros@gmail.com>
2022-04-26 00:52:52 +0300

    movint to ch 7

---
+++ src/Ch7a.purs
@@ -0,0 +1,42 @@
+module Ch7a where 
+
+import Data.Eq (class Eq)
+import Data.Ord (class Ord, Ordering(..), compare)
+import Effect (Effect)
+import Effect.Console (log)
+import Prelude (Unit, discard, show, (==), ($), (<), (>), (<=), (||))
+
+data Maybe a = Nothing | Just a  -- the order matters on deriving the Ord typeclass
+
+instance eqMaybe :: Eq a => Eq (Maybe a) where
+  eq Nothing Nothing = true
+  eq (Just x) (Just y) = x == y
+  eq _ _ = false
+
+instance ordMaybe :: Ord a => Ord (Maybe a) where
+  compare Nothing Nothing = EQ
+  compare (Just x) (Just y) = compare x y
+  compare Nothing _ = LT
+  compare _ Nothing = GT
+
+greaterThanOrEq :: forall a . Ord a => a -> a -> Boolean
+greaterThanOrEq x y = cmp == GT || cmp == EQ where cmp = compare x y
+
+infixl 4 greaterThanOrEq as >=
+
+test :: Effect Unit
+test = do
+  log $ show $ Just 5 == Just 5 -- COMPILER ERROR!! 
+  log $ show $ Just 5 == Just 2 
+  log $ show $ Just 5 == Nothing 
+  log $ show $ Nothing == Just 5 
+  log $ show $ Nothing == (Nothing :: Maybe Unit) 
+  log "------------------" 
+  log $ show $ Just 1 < Just 5 -- COMPILER ERROR!! 
+  log $ show $ Just 5 <= Just 5 
+  log $ show $ Just 5 > Just 10 
+  log $ show $ Just 10 >= Just 10 
+  log $ show $ Just 99 > Nothing 
+  log $ show $ Just 99 < Nothing 
+
+
