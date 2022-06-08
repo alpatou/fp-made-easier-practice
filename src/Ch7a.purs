288c8b76e55ae5bf9bb7aba308e64109dd7becfc
patal <patourasalexandros@gmail.com>
2022-06-08 23:47:56 +0300

    wip

--- src/Ch7a.purs
+++ src/Ch7a.purs
@@ -1,13 +1,23 @@
 module Ch7a where 
 
 import Data.Eq (class Eq)
+import Data.Generic.Rep (class Generic)
 import Data.Ord (class Ord, Ordering(..), compare)
+import Data.Show (class Show)
+import Data.Show.Generic (genericShow)
 import Effect (Effect)
 import Effect.Console (log)
-import Prelude (Unit, discard, show, (==), ($), (<), (>), (<=), (||))
+import Prelude (Unit, discard, show, ($), (<), (<=), (==), (>), (||))
 
-data Maybe a = Nothing | Just a  -- the order matters on deriving the Ord typeclass
+data Maybe a = Nothing | Just a
+derive instance eqMaybe :: Eq a => Eq (Maybe a)
+derive instance ordMaybe :: Ord a => Ord (Maybe a)
+derive instance genericMaybe :: Generic (Maybe a) _
 
+instance showMaybe :: Show a => Show (Maybe a) where
+  show = genericShow
+
+{-
 instance eqMaybe :: Eq a => Eq (Maybe a) where
   eq Nothing Nothing = true
   eq (Just x) (Just y) = x == y
@@ -19,10 +29,17 @@ instance ordMaybe :: Ord a => Ord (Maybe a) where
   compare Nothing _ = LT
   compare _ Nothing = GT
 
+instance showMaybe :: Show a => Show (Maybe a) where
+  show Nothing = "Nothing"
+  show (Just a) = "(Just " <> show a <> ")"
+-}
+
 greaterThanOrEq :: forall a . Ord a => a -> a -> Boolean
 greaterThanOrEq x y = cmp == GT || cmp == EQ where cmp = compare x y
 
 infixl 4 greaterThanOrEq as >=
+
+
 
 test :: Effect Unit
 test = do
@@ -38,5 +55,7 @@ test = do
   log $ show $ Just 10 >= Just 10 
   log $ show $ Just 99 > Nothing 
   log $ show $ Just 99 < Nothing 
+  log $ show $ Just "abc" 
+  log $ show $ (Nothing :: Maybe Unit) 
 
 
--- src/Main.purs
+++ src/Main.purs
@@ -4,8 +4,6 @@ import Prelude
 
 import Effect (Effect)
 
-import Ch5 as Ch5
-
 import Ch7a as Ch7a
 
 main :: Effect Unit
