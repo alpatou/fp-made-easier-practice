7239c339a6c8b891fb19c758e302f9dc6b916724
l l <l@l.l>
2022-09-15 21:47:05 +0300

    ch9

--- src/Ch9.purs
+++ src/Ch9.purs
@@ -1,7 +1,32 @@
 module Ch9 where
-import Prelude (Unit) 
+import Prelude (Unit, class Show, class Eq, ($), discard, show) 
 import Effect (Effect)
 import Effect.Console (log)
+import Data.Generic.Rep (class Generic)
+import Data.Show.Generic (genericShow)
+
 test :: Effect Unit
 test = do
-log "placeholder" 
+    log $ show $ ATrue <> ATrue
+    log $ show $ ATrue <> AFalse
+    log $ show $ AFalse <> AFalse
+
+
+class Semigroup a where 
+    append :: a -> a -> a
+
+infixr 5 append as <>
+
+
+class Semigroup m <= Monoid m where
+    mempty :: m
+
+data AndBool = AFalse | ATrue  
+
+derive instance eqAndBool :: Eq AndBool
+derive instance genericAndBool :: Generic AndBool _
+
+
+
+instance showAndBool :: Show AndBool where
+    show = genericShow
